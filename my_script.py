b1f0160bdfa73963402ca50d5d4ca45b55527605
Kfiros94 <104263739+kfiros94@users.noreply.github.com>
2025-05-30 09:07:22 +0300

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 with open("jenkins_output.log", "a") as log_file:
-    log_file.write(f"[{datetime.now()}] Jenkins ran this script!\n")
+    log_file.write(f"[{datetime.now()}] Jenkins ran this script Hello!\n")
